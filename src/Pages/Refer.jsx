5687b25ce4ef60eed225237865cd893b74dab61c
Ahmad4433 <aliraza126005#gmail.com>
2024-05-06 16:32:36 +0500

    DsdS

--- src/Pages/Refer.jsx
+++ src/Pages/Refer.jsx
@@ -10,7 +10,6 @@ import Decroation from "../assets/Images/image7.png";
 import Bell from "../assets/Images/bell.png";
 import Group from "../assets/Images/Group.svg";
 
-
 const Refer = () => {
   const navigate = useNavigate();
 
@@ -20,92 +19,28 @@ const Refer = () => {
 
   return (
     <>
-      <div className="container-fluid background-container refer_body">
+      <div className=" background-container refer_body">
         <img src={Group} alt="" className="grp_img" />
         <div className="switch-container">
           <div className="logo_container">
             <img className="header_logo" src={logo} alt="" />
-            <span onClick={()=>navigate('/')} className="header_logo_text">PurposeFindr</span>
+            <span onClick={() => navigate("/")} className="header_logo_text">
+              PurposeFindr
+            </span>
           </div>
         </div>
-        <div className="refer_container">
-          <div className=" container">
-            <div className="row">
-              <div className="col">
-                {/* <img src={Decroation} alt="" className="grp_imge" /> */}
-                <img src={Bell} alt="" className="grp_imges" />
-                <div className="d-flex justify-content-center">
-                  <p className="refer_title">
-                    Thank you for joining our waitlist as an individual. Keep an
-                    eye on your inbox, You will be the first to know when we
-                    launch. 🥳
-                  </p>
-                </div>
-                {/* <div className='refer_subtitle'>
-                                Refer your friends too! the more the merrier, right? 🥳
-                            </div> */}
-                {/* <div className='d-flex justify-content-center'>
-                                <form action="">
-                                    <div className='name_refer_input'>
-                                        <input type="text" placeholder='http://purposefindr.com/?ref=useername' className='url_input' />
-                                        <button className='copy_btn'>
-                                            Copy link 🔗
-                                        </button>
-                                    </div>
-                                    <div className='d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block'>
-                                        <div className='d-flex refer_btn_body gap-4'>
-                                            <button className='share_btn'>
-                                                Share on X/Twitter
-                                            </button>
-                                            <button className='linkedin_btn'>
-                                                Share on LinkedIn
-                                            </button>
-                                        </div>
-                                    </div>
-                                    <div className='d-flex justify-content-center mt-4'>
-                                        <button className='share_btn d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none'>
-                                            Share on X/Twitter
-                                        </button>
-                                    </div>
-                                    <div className='d-flex justify-content-center mt-3'>
-                                        <button className='linkedin_btn d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none'>
-                                            Share on LinkedIn
-                                        </button>
-                                    </div>
-                                    <p className='footer_copyright m-0' style={{ paddingTop: "115px", fontSize: "12px" }}>©️ 2023 PurposeFindr Inc - All rights reserved </p>
-                                </form>
-                            </div> */}
-              </div>
-            </div>
-          </div>
+        <div className="refer_container ">
+          <img src={Bell} alt="" className="grp_imges" />
+          <p className="refer_title">
+            Thank you for joining our waitlist as an individual. Keep an eye on
+            your inbox, You will be the first to know when we launch. 🥳
+          </p>
         </div>
       </div>
-
-      <Offcanvas
-        show={show}
-        onHide={handleClose}
-        placement="top"
-        style={{ background: "black" }}
-      >
-        <div className="d-flex justify-content-end pe-3">
-          <button type="button" className="close_btn" onClick={handleClose}>
-            <IoIosClose />
-          </button>
-        </div>
-        <ul className="link_style gap-3 pb-3">
-          <li className="link-item">
-            <NavLink className="link-style">Features</NavLink>
-          </li>
-          <li className="link-item">
-            <NavLink className="link-style">How it works</NavLink>
-          </li>
-          <li className="link-item">
-            <NavLink className="link-style">Support</NavLink>
-          </li>
-        </ul>
-      </Offcanvas>
     </>
   );
 };
 
 export default Refer;
+
+// container-fluid background-container
